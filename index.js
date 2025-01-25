f24965aa99e4e45d3a3593e99b89cd3fe4900718
jes-che <jessicachendeveloper@gmail.com>
2025-01-24 21:55:07 -0800

    Build logic for rendering movies

--- index.js
+++ index.js
@@ -8,27 +8,30 @@ const fetchData = async (searchTerm) => {
         }
     });
 
-    console.log(response.data);
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const debounce = (func, delay = 1000) => {
-    let timeoutId;
-    return (...args) => { // return (...args)?
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            func.call(null, ...args); // func.apply(null, args)?
-        }, delay);
-    };
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+
+    for (let movie of movies) {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+        `;
+
+        document.querySelector('#target').appendChild(div);
+    }
 };
 
-const onInput = event => {
-    fetchData(event.target.value);
-};
-
-input.addEventListener('click', debounce(onInput, 1000));
+input.addEventListener('click', debounce(onInput, 2000));
         
 fetchData();
